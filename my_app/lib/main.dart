5453b906acec102685ec93df10c529b36ec9f8da
Martin Gelev <martygelev@gmail.com>
2019-07-01 17:23:03 +0300

    statefulWidget added

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -4,19 +4,50 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('EasyList 1'),
-          backgroundColor:Color.fromRGBO(233, 88, 67, 8),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/2.jpg'),
-          Text('Food Paradice')
-        ],),),
+            title: Text('EasyList 1'),
+            backgroundColor: Color.fromRGBO(233, 88, 67, 8)),
+        body: Column(children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              onPressed: () {
+                setState(() {
+                   _products.add('Food');
+                });
+              },
+              child: Text('Add Product'),
+            ),
+          ),
+          Column(
+            children: this
+                ._products
+                .map((element) => Card(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset(
+                              '/Users/martingelev/Flutter/Flutter-App/my_app/assets/2.jpg'),
+                          Text(element)
+                        ],
+                      ),
+                    ))
+                .toList(),
+          )
+        ]),
       ),
     );
   }
